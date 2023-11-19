68409742b2ae5d56e29e3b6563ee371e1ad5d232
yusufhidayatulloh12 <ysfhidayatullo12@gmail.com>
2023-11-20 01:12:05 +0700

    text center

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -1,6 +1,7 @@
 // pages/index.js
 function Login() {
-    return <div>Hello, World!</div>;
+    return <div className="flex items-center justify-center text-blue-700" 
+    >Hello, World!</div>;
   }
   
   export default Login;
